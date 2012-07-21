780cb7df60e5b4f6e7eacf0b5757adf4f12ea0d3
DasBrieger <jwbrieger@gmail.com>
2012-07-21 16:15:48 -0700

    one key working

--- arduinoKeys/arduinoKeys.ino
+++ arduinoKeys/arduinoKeys.ino
@@ -6,46 +6,42 @@
  */
 
 String toSend = "";         // a string to hold incoming data
-int numkeys=1;
+int numKeys=1;
 int startKey=2;
 
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
-  for(int i =)
+  int currentKey= startKey;
+  pinMode(startKey, INPUT);
+  pinMode(startKey+1, INPUT);
+  
+  /*
+  for(int i =0; i<numKeys;i++){
+    
+    pinMode(2*i+startKey, INPUT);
+    //pullupResistor
+    pinMode(2*i+1+startKey, INPUT);
+    digitalWrite(2*i+1+startKey, HIGH);
+  }
+  */
 }
 
 void loop() {
+  digitalWrite(3, HIGH);
+  if(digitalRead(2)==LOW){
+   Serial.println("1T"); 
+  }
   toSend= "";
-  
-  if (stringComplete) {
-    Serial.println(inputString); 
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-  }
+  /*
+  for(int i = 0; i<numKeys; i++){
+    int current = 2*i+startKey;
+    char chCurrent = (char)current;
+    
+    if(digitalRead(current)==LOW){
+      Serial.write(chCurrent);
+      Serial.write('1');
+    }  
+  }*/
 }
-
-/*
-  SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read(); 
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    } 
-  }
-}
-
-
